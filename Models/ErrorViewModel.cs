764636e61394629d2424ab2cc24f4d294caac960
PaolaMoncayo <87495121+PaolaMoncayo@users.noreply.github.com>
2023-11-27 00:43:43 -0500

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PaginaWeb_Mascotas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PaginaWeb_Mascotas.Models
+{
+    public partial class Usuario
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo Nombre es requerido.")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El campo Apellido es requerido.")]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "El campo Dirección es requerido.")]
+        public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "El campo Teléfono es requerido.")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "El número de teléfono debe tener 10 dígitos.")]
+        public string Telefono { get; set; }
+    }
+}
